9d84cfc259b23cd68041b6a30a468759ad4acec5
Chinmoy Kalita <chinmoykalita210@gmail.com>
2024-02-27 00:19:40 +0530

    basic setup

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
 
+//For env File 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+app.get('/', (req: Request, res: Response) => {
+  res.send({"status": "ok"});
+});
+
 app.listen(port, () => {
-  console.log(`Server is started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
