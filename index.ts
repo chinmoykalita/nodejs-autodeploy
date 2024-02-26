870e29c8b7571ce5a73cfd953390dc9b606dc856
Chinmoy Kalita <chinmoykalita210@gmail.com>
2024-02-27 00:02:39 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is started at http://localhost:${port}`);
+});
